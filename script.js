b68e8611ef7bc5c0db4831574b575e6df1f4c407
Axizeaus <axizeaus@gmail.com>
2022-07-25 06:45:29 +0630

    disco mode working now

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const slider = document.getElementById('slider');
 const sliderVal = document.getElementById('slider-number');
 const eraser = document.getElementById('eraser');
 const reset = document.getElementById('reset');
+const disco = document.getElementById('disco');
+const stopDisco = document.getElementById('stopDisco');
 
 // events 
 colorInput.oninput = (e) => setColor(e.target.value);
@@ -20,6 +22,8 @@ slider.onchange = (e) => updateSlider(e);
 eraser.onclick = () => setColor('white');
 eraser.onmousedown = () => changeColorInput();
 reset.onclick = (e) => resetGrid(e);
+disco.onclick = () => discoModeInitiate();
+stopDisco.onclick = () => clearInterval(myInterval);
 
 // function calls
 window.onload = () =>{
@@ -28,6 +32,33 @@ window.onload = () =>{
 }
 
 //functions 
+
+function discoModeInitiate(){
+    list = container.childNodes
+    containerLength = container.childNodes.length
+    for(let i = 0; i<containerLength; i++){
+        check = list[i].className
+        if (check === 'active'){
+            // window.setInterval(discoMode,1000);
+            myInterval = setInterval(() => {
+                num = Math.floor(Math.random() * 255);
+                console.log(num);
+                list[i].style.backgroundColor = `rgb(${255-num},${num},${num})`;
+            }, 100)
+            clearInterval(myInterval);
+            // vg = list[i].style.backgroundColor
+            // vg = '#aaaaaa'
+            // discoMode(list[i].style.backgroundColor)
+        }
+    }
+}
+
+
+
+
+function updateSlider(){
+    // update slider code here
+}
 
 function changeColorInput(){
     colorInput.value = '#ffffff'
@@ -42,6 +73,9 @@ function setMode(val){
 }
 
 function changeColor(e){
+    e.target.classList.remove('inactive');
+    e.target.classList.add('active');
+    
     e.target.style.backgroundColor = colorNow;
 }
 
@@ -54,7 +88,7 @@ function makeGrid(num){
     let total = num * num;
     for (let i = 0; i < total; i++ ){
         let div = document.createElement('div')
-        div.classList.add('grid-item');
+        div.classList.add('inactive');
         div.addEventListener('mousedown', changeColor);
         container.appendChild(div);
     }
