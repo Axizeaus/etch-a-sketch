e7868eb830bcba6e89dae5f53182a72c00c8e3fa
Axizeaus <axizeaus@gmail.com>
2022-07-25 07:13:27 +0630

    Disco mode is working and added Disco mode end

--- script.js
+++ script.js
@@ -1,100 +1,101 @@
 // default values
-const defaultColor = document.getElementById('color').value;
-const defaultMode = 'colour';
+const defaultColor = document.getElementById("color").value;
+const defaultMode = "colour";
 const defaultNum = 18;
 
-// variables 
+// variables
 let colorNow = defaultColor;
 let mode = defaultMode;
 
-// selectors 
-const colorInput = document.getElementById('color');
-const slider = document.getElementById('slider');
-const sliderVal = document.getElementById('slider-number');
-const eraser = document.getElementById('eraser');
-const reset = document.getElementById('reset');
-const disco = document.getElementById('disco');
-const stopDisco = document.getElementById('stopDisco');
+// selectors
+const colorInput = document.getElementById("color");
+const slider = document.getElementById("slider");
+const sliderVal = document.getElementById("slider-number");
+const eraser = document.getElementById("eraser");
+const reset = document.getElementById("reset");
+const disco = document.getElementById("disco");
+const stopDisco = document.getElementById("stopDisco");
 
-// events 
+// events
 colorInput.oninput = (e) => setColor(e.target.value);
 slider.onchange = (e) => updateSlider(e);
-eraser.onclick = () => setColor('white');
+eraser.onclick = () => setColor("white");
 eraser.onmousedown = () => changeColorInput();
 reset.onclick = (e) => resetGrid(e);
 disco.onclick = () => discoModeInitiate();
-stopDisco.onclick = () => clearInterval(myInterval);
+stopDisco.onclick = () => discoModeEnd();
 
 // function calls
-window.onload = () =>{
-    makeGrid(defaultNum);
-    mode = 'colour'
+window.onload = () => {
+  makeGrid(defaultNum);
+  mode = "colour";
+};
+
+//functions
+
+function discoModeInitiate() {
+  list = container.childNodes;
+  containerLength = container.childNodes.length;
+  for (let i = 0; i < containerLength; i++) {
+    check = list[i].className;
+    if (check === "active") {
+      myInterval = setInterval(() => {
+        num = Math.floor(Math.random() * 255);
+        console.log(num);
+        list[i].style.backgroundColor = `rgb(${255 - num},${num},${num})`;
+        // console.log( '===>' + myInterval);
+      }, 100);
+    } 
+  }
 }
 
-//functions 
+function discoModeEnd() {
+  let id = 10000;
+  while (id > 0){
+    window.clearTimeout(id);
+    id--;
+  }
+} 
 
-function discoModeInitiate(){
-    list = container.childNodes
-    containerLength = container.childNodes.length
-    for(let i = 0; i<containerLength; i++){
-        check = list[i].className
-        if (check === 'active'){
-            // window.setInterval(discoMode,1000);
-            myInterval = setInterval(() => {
-                num = Math.floor(Math.random() * 255);
-                console.log(num);
-                list[i].style.backgroundColor = `rgb(${255-num},${num},${num})`;
-            }, 100)
-            clearInterval(myInterval);
-            // vg = list[i].style.backgroundColor
-            // vg = '#aaaaaa'
-            // discoMode(list[i].style.backgroundColor)
-        }
-    }
+function updateSlider() {
+  // update slider code here
 }
 
-
-
-
-function updateSlider(){
-    // update slider code here
+function changeColorInput() {
+  colorInput.value = "#ffffff";
 }
 
-function changeColorInput(){
-    colorInput.value = '#ffffff'
+function setColor(val) {
+  colorNow = val;
 }
 
-function setColor(val){
-    colorNow = val;
+function setMode(val) {
+  mode = val;
 }
 
-function setMode(val){
-    mode = val;
+function changeColor(e) {
+  e.target.classList.remove("inactive");
+  e.target.classList.add("active");
+
+  e.target.style.backgroundColor = colorNow;
 }
 
-function changeColor(e){
-    e.target.classList.remove('inactive');
-    e.target.classList.add('active');
-    
-    e.target.style.backgroundColor = colorNow;
-}
-
-function makeGrid(num){
-    container.setAttribute(
-        "style",
-        `grid-template-columns:repeat(${num}, ${container.clientWidth / num}px);
+function makeGrid(num) {
+  container.setAttribute(
+    "style",
+    `grid-template-columns:repeat(${num}, ${container.clientWidth / num}px);
         grid-template-rows:repeat(${num}, ${container.clientHeight / num}px)`
-    );
-    let total = num * num;
-    for (let i = 0; i < total; i++ ){
-        let div = document.createElement('div')
-        div.classList.add('inactive');
-        div.addEventListener('mousedown', changeColor);
-        container.appendChild(div);
-    }
+  );
+  let total = num * num;
+  for (let i = 0; i < total; i++) {
+    let div = document.createElement("div");
+    div.classList.add("inactive");
+    div.addEventListener("mousedown", changeColor);
+    container.appendChild(div);
+  }
 }
 
-function resetGrid(){
-    container.innerHTML = '';
-    makeGrid(defaultNum);
+function resetGrid() {
+  container.innerHTML = "";
+  makeGrid(defaultNum);
 }
